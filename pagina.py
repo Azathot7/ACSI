d672d37dcefe361093cff9b77a360740384197a8
Azathot7 <42989276+Azathot7@users.noreply.github.com>
2022-11-13 21:48:33 -0500

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -24,27 +24,27 @@ with st.container():
 name=option 
 iamge=db.dcm_img(name)
 iamge2=np.array(iamge)
-#iamge3=red.predecir(iamge2)
+iamge3=red.predecir(iamge2)
 
-#with st.container():
-#    st.write("---")
+with st.container():
+    st.write("---")
 
-#    left_column, right_column = st.columns(2)
-#    with left_column:
-#        st.write("Original")
-#        st.image(iamge,width=600)
-#    with right_column:
-#        st.write("Diagóstico")
-#        st.image(iamge3,width=600)
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.write("Original")
+        st.image(iamge,width=600)
+    with right_column:
+        st.write("Diagóstico")
+        st.image(iamge3,width=600)
 
-#with st.container():
-#    file = st.file_uploader("Seleccione el archivo a subir")
-#    if file is not None:
-#        result = st.button("Subir archivo")
-#        if result:
-#            bytes_file = file.getvalue()
-#            db.insert_dcm("prueba.dcm",bytes_file)  
-#            pyautogui.hotkey("ctrl","F5")
+with st.container():
+    file = st.file_uploader("Seleccione el archivo a subir")
+    if file is not None:
+        result = st.button("Subir archivo")
+        if result:
+            bytes_file = file.getvalue()
+            db.insert_dcm("prueba.dcm",bytes_file)  
+            pyautogui.hotkey("ctrl","F5")
             
 with st.container():    
     cuadro=db.dcm_info(name)
